9386fba1bbb205ba37a209ef7cafa81fe9e3db5b
jnpryke <jnpryke@gmail.com>
2016-10-13 03:12:39 -0400

    add comments about the text color of the action bar title

--- app/src/main/java/com/example/john/listviewtest3/MainActivity.java
+++ app/src/main/java/com/example/john/listviewtest3/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
@@ -115,6 +117,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.version) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public static void minusUnitCostFromIPCs(int position){
