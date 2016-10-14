feac086eb777fb4fb77b66ed9a45abf9b89a640c
jnpryke <jnpryke@gmail.com>
2016-10-14 02:22:48 -0400

    added a&a versions

--- app/src/main/java/com/example/john/listviewtest3/UnitInfo.java
+++ app/src/main/java/com/example/john/listviewtest3/UnitInfo.java
@@ -17,7 +17,7 @@ public class UnitInfo {
     }
 
     public void setVersion(int version){
-        //version 0 = Axis and Allies 1942 SE
+        //version 0 = Axis and Allies 1942 second edition
         if(version == 0){
             unitQuantityArray = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0};
             unitPriceArray = new int[]{3,4,6,5,10,12,6,7,8,12,20,14,15,1};
@@ -46,8 +46,41 @@ public class UnitInfo {
                     {6,2,1,2},{7,0,0,2},{8,2,2,2},{16,4,4,2},{12,1,2,2}};
 
         }
+        //!complete
+        //version 2 = Axis and Allies 1940 Pacific/Europe second edition
+        if(version == 2){
+            unitQuantityArray = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            unitPriceArray = new int[]{3,4,4,6,5,10,11,12,6,7,8,12,20,16,30,12,1,20,15,15};
+            unitNamesStringArray = new String[]{"Infantry", "Artillery", "Mechanized Infantry", "Tank", "AAA", "Fighter",
+                    "Tactical Bomber", "Strategic Bomber", "Submarine", "Transport", "Destroyer", "Cruiser", "Battleship",
+                    "Aircraft Carrier", "Major Industrial Complex", "Minor Industrial Complex","Repair Industrial Complex",
+                    "Upgrade Industrial Complex","Air Base","Naval Base"};
+            //!complete
+            AnAImages = new int[] {R.drawable.infantry, R.drawable.artillery, R.drawable.tank, R.drawable.aaa,
+                    R.drawable.fighter,R.drawable.bomber, R.drawable.submarine, R.drawable.transport,
+                    R.drawable.destroyer, R.drawable.cruiser, R.drawable.battleship,
+                    R.drawable.aircraftcarrier, R.drawable.industrialcomplex, R.drawable.repairindustrialcomplex};
+            unitStatsInfoArray = new int[][]{{3,1,2,1},{4,2,2,1},{4,1,2,2},{6,3,3,2},{5,0,0,1},{10,3,4,4},{11,3,3,4},
+                    {12,4,1,6},{6,2,1,2},{7,0,0,2},{8,2,2,2},{12,3,3,2},{20,4,4,2},{16,1,2,2},{30,0,0,0}, {15,0,0,0},
+                    {1,0,0,0},{20,0,0,0},{15,0,0,0},{15,0,0,0}};
 
-        //version 2 = Axis and Allies 1940 Pacific/Europe SE
+        }
+
+        //version 3 = Axis and Allies 1942 Spring
+        if(version == 3){
+            unitQuantityArray = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            unitPriceArray = new int[]{3,4,6,5,10,12,6,7,8,12,20,14,15,1};
+            unitNamesStringArray = new String[]{"Infantry", "Artillery", "Tank", "AAA", "Fighter", "Bomber",
+                    "Submarine", "Transport", "Destroyer", "Cruiser", "Battleship", "Aircraft Carrier",
+                    "Industrial Complex", "Repair Industrial Complex"};
+            AnAImages = new int[] {R.drawable.infantry, R.drawable.artillery, R.drawable.tank, R.drawable.aaa,
+                    R.drawable.fighter,R.drawable.bomber, R.drawable.submarine, R.drawable.transport,
+                    R.drawable.destroyer, R.drawable.cruiser, R.drawable.battleship,
+                    R.drawable.aircraftcarrier, R.drawable.industrialcomplex, R.drawable.repairindustrialcomplex};
+            unitStatsInfoArray = new int[][]{{3,1,2,1},{4,2,2,1},{5,3,3,2},{6,0,0,1},{10,3,4,4},{12,4,1,6},
+                    {6,2,1,2},{7,0,0,2},{8,2,2,2},{12,3,3,2},{20,4,4,2},{14,1,2,2},{15,0,0,0},{1,0,0,0}};
+
+        }
     }
 
     public int[] getAnAImages() {
